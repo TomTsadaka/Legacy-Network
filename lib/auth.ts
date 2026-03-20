d4e9c6e8f122d5edf91f6de5d4d6f5d3da0fd350
Tom Tsadaka <tomtsadaka@gmail.com>
2026-03-20 05:59:46 +0200

    Remove PrismaAdapter for credentials auth to work

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,7 +5,6 @@ import Credentials from 'next-auth/providers/credentials';
 import prisma from './prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   session: {
     strategy: 'jwt',
   },
